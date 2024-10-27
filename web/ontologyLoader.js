7ba993a62b636e4561abe3baaf44ad28502143f0
chrlesur <christophe.lesur@cloud-temple.com>
2024-10-27 19:12:16 +0100

    0.1.5 UI Update

--- web/ontologyLoader.js
+++ web/ontologyLoader.js
@@ -1,23 +1,47 @@
 // ontologyLoader.js
 import { uploadOntology } from './api.js';
+import { showErrorMessage } from './main.js';
 
 const uploadForm = document.getElementById('upload-form');
+const uploadModal = document.getElementById('upload-modal');
+const uploadProgress = document.getElementById('upload-progress');
 
 // Initialisation du chargeur d'ontologie
 export function initOntologyLoader() {
     uploadForm.addEventListener('submit', handleUpload);
+    document.querySelector('.close').addEventListener('click', closeModal);
 }
 
 // Gestion du chargement de fichiers
 async function handleUpload(event) {
     event.preventDefault();
     const formData = new FormData(uploadForm);
+    
     try {
+        uploadForm.classList.add('hidden');
+        uploadProgress.classList.remove('hidden');
+        
         const response = await uploadOntology(formData);
         console.log('Ontologie chargée avec succès:', response);
-        // TODO: Mettre à jour l'interface utilisateur après le chargement réussi
+        
+        // Fermer le modal après un court délai
+        setTimeout(() => {
+            closeModal();
+            // TODO: Mettre à jour l'interface utilisateur après le chargement réussi
+        }, 1000);
     } catch (error) {
         console.error('Erreur lors du chargement de l\'ontologie:', error);
         showErrorMessage('Une erreur est survenue lors du chargement de l\'ontologie.');
+    } finally {
+        uploadForm.classList.remove('hidden');
+        uploadProgress.classList.add('hidden');
     }
 }
+
+function closeModal() {
+    uploadModal.style.display = 'none';
+    uploadForm.reset();
+}
+
+// Exporter la fonction closeModal pour l'utiliser ailleurs si nécessaire
+export { closeModal };
